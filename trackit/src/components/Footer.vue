c07277a2645d6a6c811023e867bab464cce471d6
23026475 <143639427+23026475@users.noreply.github.com>
2025-10-10 20:53:40 +0200

    Delete trackit directory
    Replacing it with a lighter frame work. The structure will still remain the same

--- trackit/src/components/Footer.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <footer class="w-full bg-[var(--color-background)] text-[var(--color-text)] p-4 text-center border-t border-[var(--color-border)]">
-      <p>&copy; 2025 TrackIt. All rights reserved.</p>
-    </footer>
-</template>
-  
